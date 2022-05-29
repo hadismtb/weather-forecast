f1322a84005d785c10d5d3cd9188a50d6d0fa756
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-29 18:55:18 +0430

    import weatherInfo component to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Main from "./components/Main";
 import { Provider} from "react-redux";
 import store from "./redux/store";
+
+//COMPONENTS
+import Main from "./components/Main";
+import WeatherInfo from "./components/WeatherInfo";
 
 function App() {
   return (
      <Provider store={store}>
         <div className="app">
             <Main/>
+            <WeatherInfo/>
         </div>
      </Provider>
   );
