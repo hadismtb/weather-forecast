17ab1d07d11bedfcabea5c6e858c11b44abd47cf
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-29 19:01:18 +0430

    create navbar list with jsx

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <ul>
+            <li>Weather</li>
+            <li>Alerts</li>
+            <li>Map</li>
+            <li>Satellite</li>
+            <li>News</li>
+        </ul>
     );
 };
 
