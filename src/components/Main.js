ca436c1601a6699e973691dd5946e1ab0cb0bc8f
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-29 15:57:36 +0430

    styled to Main component with external stylesheet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import styles from "./Main.module.css"
 
 const Main = () => {
     return (
-        <div>
-            <header>
+        <div className={styles.container}>
+            <header className={styles.header}>
                 <h1>forecast</h1>
-                <div>
+                <div className={styles.locationContainer}>
                     <span><i className="fas fa-map-marker-alt"></i></span>
-                    <div>
+                    <div className={styles.cityName}>
                         <p>Current Location</p>
                         <p>Tehran, Iran</p>
                     </div>
                 </div>
             </header>
-            <div>
+            <div className={styles.searchBox}>
                 <h2>The Only Weather Forecast You Need</h2>
-                <div></div>
+                <div className={styles.horizontalRow}></div>
                 <form>
-                    <div>
-                        <input type="text"/>
+                    <div className={styles.inputContainer}>
+                        <input type="text" placeholder="Enter location"/>
                         <button type="submit"><i className="fas fa-search"></i></button>
-                        <span>
-                            <i className="fas fa-location-arrow"></i>
-                        </span>
                     </div>
+                    <span className={styles.liveLocation}>
+                            <i className="fas fa-location-arrow"></i>
+                    </span>
                 </form>
             </div>
         </div>
