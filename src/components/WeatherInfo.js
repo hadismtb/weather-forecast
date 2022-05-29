651ac705e388977e136167ebb7d62667b54c7ae7
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-29 18:58:51 +0430

    import navbar component to weatherInfo component

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from "./Navbar";
 
 const WeatherInfo = () => {
     return (
         <div>
-            
+            <Navbar/>
         </div>
     );
 };
