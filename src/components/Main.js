139d6d571a75148107c3a1fc83a109170b3a4d76
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-30 19:20:36 +0430

    create a feature for getting live and searched location in location container

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,31 +8,41 @@ import {fetchWeather} from "../redux/weatherData/weatherDataAction";
 const Main = () => {
     const API_KEY = "65ab2c747b41b6ef08230971719b640f";
     const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast?";
+    const [searched, setSearched] = useState(false);
 
     const dispatch = useDispatch();
+
+    //GET CURRENT LOCATION WITH LOCATION STATE
     const state = useSelector(state => state.locationState);
     const {loading, city, country, error} = state;
     const [cityName, setCityName] = useState("");
 
+    //GET CURRENT LOCATION BASED ON SEARCH
+    const weatherState = useSelector(state => state.weatherState);
+    const {data, error: weatherError, loading: weatherLoading} = weatherState;
+
     useEffect(() => {
-        dispatch(fetchLocation())
+        dispatch(fetchLocation());
     }, [])
 
     const handleSubmit = e => {
         e.preventDefault();
 
         const url = `${BASE_URL}q=${cityName}&appid=${API_KEY}&units=metric&cnt=5&exclude=hourly,minutely`;
-        dispatch(fetchWeather(url));
-        setCityName("")
+        if (cityName.trim()) {
+            dispatch(fetchWeather(url));
+            setCityName("")
+            setSearched(true);
+        }
     }
 
     const handleClick = () => {
         navigator.geolocation.getCurrentPosition(function(position) {
 
             const {latitude, longitude} = position.coords;
-            const url = `${BASE_URL}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&cnt=5&exclude=hourly,minutely`
+            const url = `${BASE_URL}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&cnt=5&exclude=hourly,minutely`;
 
-            dispatch(fetchWeather(url))
+            dispatch(fetchWeather(url));
         });
     }
 
@@ -44,12 +54,22 @@ const Main = () => {
                     <span><i className="fas fa-map-marker-alt"></i></span>
                     <div className={styles.cityName}>
                         <p>Current Location</p>
-                        <p>{
-                            loading ?
-                                "loading..." :
-                                !error && !loading ?
-                                    `${city}, ${country}` :
-                                    city
+                        <p>
+                            {
+                                searched ?
+                                    weatherLoading ?
+                                        "loading..." :
+                                            !weatherError ? `${data.city.name + ", " + data.city.country}` :
+                                                "Unknown" :
+                                    ""
+                            }
+                            {!searched ?
+                                loading ?
+                                    "loading..." :
+                                    !error && !loading ?
+                                        `${city}, ${country}` :
+                                        city :
+                                ""
                         }</p>
                     </div>
                 </div>
