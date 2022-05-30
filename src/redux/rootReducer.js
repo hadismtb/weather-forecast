5a35ed40f0ed5bfc0db6a156ac47d63f705780b2
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-30 12:13:32 +0430

    import weather data reducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers} from "redux";
 import currentLocationReducer from "./currentLocation/currentLocationReducer";
+import weatherDataReducer from "./weatherData/WeatherDataReducer";
 
 const rootReducer = combineReducers({
     locationState: currentLocationReducer,
+    weatherState: weatherDataReducer
 })
 
 export default rootReducer;
