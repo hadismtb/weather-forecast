c7d15d64780cc499db3d6fff84d9ff340874c355
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-30 12:00:45 +0430

    create fetchWeather function to handle all action of weather data

--- src/redux/weatherData/weatherDataAction.js
+++ src/redux/weatherData/weatherDataAction.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-//api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid={API key}
-
-//API REQUIREMENTS
-const API_KEY = "65ab2c747b41b6ef08230971719b640f";
-const BASE_URL = "api.openweathermap.org/data/2.5/forecast?";
-
 const fetchWeatherRequest = () => {
     return {type: "FETCH_WEATHER_REQUEST"}
 }
@@ -19,3 +12,16 @@ const fetchWeatherFailure = error => {
     return {type: "FETCH_WEATHER_FAILURE", payload: error}
 }
 
+export const fetchWeather = url => {
+    return (dispatch) => {
+        dispatch(fetchWeatherRequest());
+        axios.get(url)
+            .then(response => {
+                const data = response.data
+                dispatch(fetchWeatherSuccess(data))
+            })
+            .catch(error => {
+                dispatch(fetchWeatherFailure("Location Not Found"))
+            })
+    }
+}
