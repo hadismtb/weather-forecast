328e2dcb864ab1fc2034f87065fb7cdcbe083eaa
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-31 15:52:19 +0430

    style to current weather component with external css file

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useSelector } from "react-redux";
 import moment from "moment";
 
+//STYLES
+import styles from "./CurrentWeather.module.css";
+
 const CurrentWeather = () => {
     const weatherState = useSelector(state => state.weatherState);
 
@@ -10,18 +13,18 @@ const CurrentWeather = () => {
     const {temp_min, temp_max, humidity} = todayData.main;
 
     return (
-        <div>
-            <h3>Today</h3>
-            <div>
-                <div>
-                    <h4>{Math.round(todayData.main.temp)}&deg;C</h4>
-                    <div>
+        <div className={styles.container}>
+            <h3 className={styles.title}>Today</h3>
+            <div className={styles.infoContainer}>
+                <div className={styles.left}>
+                    <h4 className={styles.temp}>{Math.round(todayData.main.temp)}&deg;<span>C</span></h4>
+                    <div className={styles.status}>
                         <h4>{todayData.weather[0].main}</h4>
                         <img src={` http://openweathermap.org/img/wn/${todayData.weather[0].icon}@2x.png`} alt=""/>
                     </div>
-                    <p>{moment().format(`dddd ${new Date().getDate()}, MMM YYYY`)}</p>
+                    <p className={styles.date}>{moment().format(`dddd ${new Date().getDate()}, MMM YYYY`)}</p>
                 </div>
-                <div>
+                <div className={styles.right}>
                     <p>Min-temp: <span>{Math.round(temp_min)}</span></p>
                     <p>Max-temp: <span>{Math.round(temp_max)}</span></p>
                     <p>Humidity: <span>{humidity}%</span></p>
