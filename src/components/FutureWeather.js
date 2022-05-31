619e16092b05d978f3c565629bedb0e75f855923
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-31 17:11:43 +0430

    apply some class name to element for getting styles

--- src/components/FutureWeather.js
+++ src/components/FutureWeather.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import moment from "moment";
 
-const FutureWeather = () => {
+//STYLES
+import styles from "./FutureWeather.module.css";
+
+const FutureWeather = ({data}) => {
+    const {temp} = data.main;
+    const {main:status, icon} = data.weather[0];
+    const date = data.dt_txt;
+
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <h4 className={styles.temp}>{Math.round(temp)} &deg;<span>C</span></h4>
+            <div className={styles.status}>
+                <h4>{status}</h4>
+                <img src={` http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon"/>
+            </div>
+            <p className={styles.date}>{moment(date).format("ddd hh:mm")}</p>
         </div>
     );
 };
